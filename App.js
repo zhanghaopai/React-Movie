b7b2b9cbadd317b5ac36a4cc8d9d96dab1571af8
pai_v98 <zhanghaopai@outlook.com>
2024-04-29 22:12:41 +0800

    react native & tailwind

--- App.js
+++ App.js
@@ -3,18 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <View className="bg-red-400">
+        <Text className="text-white">Hello World</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
