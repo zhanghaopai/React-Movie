be34689c34ec160e1a0097f4eda93230a6091b72
pai_v98 <zhanghaopai@outlook.com>
2024-05-01 11:05:21 +0800

    提交初始化模版

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigation from './navigation/appNavigation';
 
 export default function App() {
   return (
-    <View >
-      <View className="bg-red-400">
-        <Text className="text-white">Hello World</Text>
-      </View>
-      
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigation />
   );
 }
