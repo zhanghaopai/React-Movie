47a96bd78f3923aa67e49726ba940cd3bb702fb1
pai_v98 <zhanghaopai@outlook.com>
2024-05-01 11:22:29 +0800

    增加平台判断

--- components/screenWrapper.js
+++ components/screenWrapper.js
@@ -1,9 +1,10 @@
-import {View, Text} from 'react-native'
+import {View, Text, Platform} from 'react-native'
 import {React} from 'react'
 import { StatusBar } from 'expo-status-bar'
 
 export default function ScreenWrapper({children}) {
-    let statusBarHeight = StatusBar.currentHeight? StatusBar.currentHeight:30;
+    let osHeight = Platform.OS == "ios"? 30 : 0;
+    let statusBarHeight = StatusBar.currentHeight? StatusBar.currentHeight:osHeight;
   return (
     <View style={{paddingTop: statusBarHeight}}>
         {children}
